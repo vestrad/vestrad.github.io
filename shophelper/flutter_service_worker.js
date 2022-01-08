3f62d33264e8f4276887c0776c2faedb5123b537
vestrad <33851565+vestrad@users.noreply.github.com>
2022-01-07 21:51:14 -0500

    Add files via upload

--- shophelper/flutter_service_worker.js
+++ shophelper/flutter_service_worker.js
@@ -11,9 +11,9 @@ const RESOURCES = {
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
-"index.html": "831d725fb41a3df46f95fdb098c7e2cf",
-"/": "831d725fb41a3df46f95fdb098c7e2cf",
-"main.dart.js": "9bf0c6d6015f803a21431e3f4da883a5",
+"index.html": "22b0b98e9e30977420d15725f8d86492",
+"/": "22b0b98e9e30977420d15725f8d86492",
+"main.dart.js": "2cd2d32858d55cc24c28c17af6718481",
 "manifest.json": "b6236f34a9615965708c2bd3340b65c1",
 "version.json": "f19a4706ad2becdc3962344d33726ab8"
 };
